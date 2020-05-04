2eaee01920718ca2dba4732759dd29220b7af920
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-04 16:08:14 -0400

    add authenticate function and add extra validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,19 @@ class User < ActiveRecord::Base
 
    validates :first_name, presence: true
    validates :last_name, presence: true
-   validates :password, presence: true, length: { minimum: 4 }
+   validates :password, presence: true, length: { minimum: 8}
    validates :password_confirmation, presence: true
-   validates_uniqueness_of :email, case_sensitive: false, presence: true
+   validates_uniqueness_of :email, case_sensitive: false
+   validates :email, presence: true
+   
+
+   def self.authenticate_with_credentials(email, password)
+      user = User.find_by_email(email.strip.downcase)
+      if user && user.authenticate(password)
+          return user
+        else 
+          return nil
+        end
+   end
    
 end
