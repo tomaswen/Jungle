74fb193d0306ff27eca6ba37df02b3374edcf4a5
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-05 14:30:20 -0400

    add test to check product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "Product details", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see details of the product" do
+    # ACT
+    visit root_path
+
+    page.first( :link, 'Details').click
+    expect(page).to have_selector '.products-show'
+    save_screenshot
+
+  end
+end
